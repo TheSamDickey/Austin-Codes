e3db0ce0136c64f58909077bda49e2e00ef3ebb0
TheSamDickey 🦁 <thesamdickey@gmail.com>
2023-10-05 01:50:59 +0000

    Reorganize frontend project

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import * as ReactRouterDom from "react-router-dom";
+import "./global/index.css";
+import { Home } from "./pages/Home/Home";
+const { RouterProvider, createBrowserRouter } = ReactRouterDom;
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <Home />,
+	},
+	{
+		path: "/home",
+		element: <Home />,
+	},
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>
+);
